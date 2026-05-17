24c97206c5779f32fd387222a9b869eb657929d2
Pradipta Sarker <techshuvo@gmail.com>
2026-05-17 15:06:08 +0600

    better auth , mondgobd setup

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,16 @@
-import { client } from "@/db"; // your mongodb client
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.BETTER_AUTH_DB_URL);
+const db = client.db("pulsewire");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(client),
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client,
+  }),
 });
