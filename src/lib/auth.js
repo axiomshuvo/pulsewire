439cda8fa820ffb7558bcb4c7ce31cc044fc7e6e
Pradipta Sarker <techshuvo@gmail.com>
2026-05-20 02:24:25 +0600

    proxy [middlewire] implementation

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,3 @@
-import dns from "node:dns";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-// set google's public DNS servers to avoid potential issues with local DNS resolution in certain environments
-//   এইটা করার কারণ হলো, কিছু পরিবেশে লোকাল DNS রেজল্যুশন সমস্যা হতে পারে,
-//  তাই গুগলের পাবলিক DNS সার্ভার ব্যবহার করা হচ্ছে যাতে এই ধরনের সমস্যা এড়ানো যায়।
-
 import { betterAuth } from "better-auth";
 
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
---
+++ src/proxy.js
@@ -0,0 +1,22 @@
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+export async function proxy(request) {
+  console.log(request);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  console.log("Session data in proxy middleware:", session);
+  if (session) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/login", request.url));
+}
+
+export const config = {
+  matcher: ["/dashboard"],
+};
